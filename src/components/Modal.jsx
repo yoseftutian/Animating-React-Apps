88f13e60069d105dfbc117171c7e888f7897aca6
yosef-tutian <yoseftutian@gmail.com>
2024-12-13 14:31:11 +0200

    - Integrated `framer-motion` for smooth animations on image selection

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,14 +1,18 @@
 import { createPortal } from "react-dom";
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
 export default function Modal({ title, children, onClose }) {
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
       <motion.dialog
-        initial={{ opacity: 0, y: 30 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: 30 }}
+        variants={{
+          hidden: { opacity: 0, y: 30 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
         open
         className="modal"
       >
--- src/components/NewChallenge.jsx
+++ src/components/NewChallenge.jsx
@@ -1,8 +1,8 @@
-import { useContext, useRef, useState } from 'react';
-
-import { ChallengesContext } from '../store/challenges-context.jsx';
-import Modal from './Modal.jsx';
-import images from '../assets/images.js';
+import { useContext, useRef, useState } from "react";
+import { motion } from "framer-motion";
+import { ChallengesContext } from "../store/challenges-context.jsx";
+import Modal from "./Modal.jsx";
+import images from "../assets/images.js";
 
 export default function NewChallenge({ onDone }) {
   const title = useRef();
@@ -58,13 +58,19 @@ export default function NewChallenge({ onDone }) {
 
         <ul id="new-challenge-images">
           {images.map((image) => (
-            <li
+            <motion.li
+              variants={{
+                hidden: { opacity: 0, scale: 0.5 },
+                visible: { opacity: 1, scale: 1 },
+              }}
+              exit={{ opacity: 1, scale: 1 }}
+              transition={{ type: "spring" }}
               key={image.alt}
               onClick={() => handleSelectImage(image)}
-              className={selectedImage === image ? 'selected' : undefined}
+              className={selectedImage === image ? "selected" : undefined}
             >
               <img {...image} />
-            </li>
+            </motion.li>
           ))}
         </ul>
 
